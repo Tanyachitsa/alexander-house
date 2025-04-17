ce228a37e6c04f8dd8ebb5725b140d04e044abc5
Tanyachitsa <165687341+Tanyachitsa@users.noreply.github.com>
2025-04-17 19:17:13 +0100

    created the footer

---
+++ src/components/Footer.jsx
@@ -0,0 +1,15 @@
+export default function Footer() {
+  return (
+    <footer>
+      <p className="address-container">
+        Alexander House
+        <br />
+        10 Hanover Square Mayfair, London
+        <br />
+        W1S 1DA United Kingdom
+        <br />
+        020 7946 1234
+      </p>
+    </footer>
+  );
+}
