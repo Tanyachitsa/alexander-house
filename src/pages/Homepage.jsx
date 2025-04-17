9b275362fb389b300c279a51b69b3185d063e629
Tanyachitsa <165687341+Tanyachitsa@users.noreply.github.com>
2025-04-18 00:22:05 +0100

    updated homepage.jsx

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -12,7 +12,21 @@ export default function Homepage() {
         loop
         muted
         style={{ marginBottom: "100px" }}
-      ></video>{" "}
+      ></video>
+      <br />
+      <p className="homepage-summary">
+        <em className="homepage-summary-title">
+          <strong style={{ fontSize: "55px" }}>Alexander House </strong>is a
+          refined sanctuary for modern professionals, creatives, and discerning
+          minds.
+        </em>
+        <br />
+        Designed for those who value privacy, connection, and elevated living,
+        membership unlocks access to elegant lounges, curated dining, cultural
+        salons, wellness spaces, and luxurious overnight stays. With limited
+        memberships, tailored events, and a focus on community over crowd,
+        Alexander House is more than a club — it's your second home.
+      </p>
       <Footer />
     </>
   );
