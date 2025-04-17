64c38ae95fa0b60877346256424a4cf42d829a9e
Tanyachitsa <165687341+Tanyachitsa@users.noreply.github.com>
2025-04-18 00:50:00 +0100

    Created a file for the house information

---
+++ src/components/houseImages.jsx
@@ -0,0 +1,180 @@
+export default function HouseImages() {
+  return (
+    <>
+      <div
+        className="container text-center"
+        style={{ paddingBottom: "30px", paddingTop: "30px" }}
+      >
+        <div className="row">
+          <div className="col">
+            <img
+              src="/src/Media/houseMedia/stlucia.jpg"
+              alt="image of hotel reception"
+              className="house-img"
+            />
+            <br />
+            <h4>The Palm Pavilion </h4>
+            <h5>- St Lucia -</h5>
+            <p>
+              A tranquil escape surrounded by lush greenery and natural wood
+              finishes.
+            </p>
+          </div>
+          <div className="col">
+            <img
+              src="/src/Media/houseMedia/jordan.jpg"
+              alt="image of hotel reception"
+              className="house-img"
+            />
+            <br />
+            <h4>The Medina Room </h4>
+            <h5>- Jordan -</h5>
+            <p>Tiled elegance and intimate courtyards create a hidden haven.</p>
+          </div>
+          <div className="col">
+            <img
+              src="/src/Media/houseMedia/london.jpg"
+              alt="image of hotel reception"
+              className="house-img"
+            />
+            <br />
+            <h4>The Guilded Room </h4>
+            <h5>- London -</h5>
+            <p>
+              Golden accents and plush finishes evoke timeless elegance and
+              classic comfort.
+            </p>
+          </div>
+        </div>
+      </div>
+
+      <div
+        className="container text-center"
+        style={{ paddingBottom: "30px", paddingTop: "30px" }}
+      >
+        <div className="row">
+          <div className="col">
+            <img
+              src="/src/Media/houseMedia/oxford.jpg"
+              alt="image of hotel reception"
+              className="house-img"
+            />
+            <br />
+            <h4>The Library House </h4>
+            <h5>- Oxford -</h5>
+            <p>
+              Warm, book-lined walls and a fireplace — perfect for quiet
+              thinkers and dreamers.
+            </p>
+          </div>
+
+          <div className="col">
+            <img
+              src="/src/Media/houseMedia/morocco.jpg"
+              alt="image of hotel reception"
+              className="house-img"
+            />
+            <br />
+            <h4>The Marrakesh Terrace </h4>
+            <h5>- Morocco -</h5>
+            <p>Vibrant mosaics and lantern-lit ambiance.</p>
+          </div>
+          <div className="col">
+            <img
+              src="/src/Media/houseMedia/paris.jpg"
+              alt="image of hotel reception"
+              className="house-img"
+            />
+            <br />
+            <h4>The Artsian Studio </h4>
+            <h5>- Paris -</h5>
+            <p>
+              Textured walls, earthy palettes, and vintage furniture offer
+              handcrafted comfort.
+            </p>
+          </div>
+        </div>
+      </div>
+
+      <div
+        className="container text-center"
+        style={{ paddingBottom: "30px", paddingTop: "30px" }}
+      >
+        <div className="row">
+          <div className="col">
+            <img
+              src="/src/Media/houseMedia/lakecomo.jpg"
+              alt="image of hotel reception"
+              className="house-img"
+            />
+            <br />
+            <h4>The Roman Garden Suite </h4>
+            <h5>- Italy -</h5>
+            <p>Classical details meet lush greenery in timeless style.</p>
+          </div>
+
+          <div className="col">
+            <img
+              src="/src/Media/houseMedia/swissalps.jpg"
+              alt="image of hotel reception"
+              className="house-img"
+            />
+            <br />
+            <h4>The Glacier Loft </h4>
+            <h5>- Switzerland -</h5>
+            <p>Mountain luxury with plush linens and fur throws.</p>
+          </div>
+          <div className="col">
+            <img
+              src="/src/Media/houseMedia/zimbabwe.jpeg"
+              alt="image of hotel reception"
+              className="house-img"
+            />
+            <br />
+            <h4>The Baobab Suite </h4>
+            <h5>- Zimbabwe -</h5>
+            <p>Rooted in strength and beauty, with grounding, natural decor.</p>
+          </div>
+        </div>
+      </div>
+
+      <div className="container text-center">
+        <div className="row">
+          <div className="col">
+            <img
+              src="/src/Media/houseMedia/egypt.jpg"
+              alt="image of hotel reception"
+              className="house-img"
+            />
+            <br />
+            <h4>The Oasis Pavilion </h4>
+            <h5>- Egypt -</h5>
+            <p>Fragrant, peaceful, and filled with quiet romance.</p>
+          </div>
+          <div className="col">
+            <img
+              src="/src/Media/houseMedia/mauritius.jpg"
+              alt="image of hotel reception"
+              className="house-img"
+            />
+            <br />
+            <h4>The Hibiscus Hideaway </h4>
+            <h5>- Mauritius -</h5>
+            <p>Vibrant, floral, and intimate — a bold yet calming retreat.</p>
+          </div>
+          <div className="col">
+            <img
+              src="/src/Media/houseMedia/tuscany.jpg"
+              alt="image of hotel reception"
+              className="house-img"
+            />
+            <br />
+            <h4>The Tuscan Retreat </h4>
+            <h5>- Italy -</h5>
+            <p>Warm stone walls, rustic charm, and golden light..</p>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
