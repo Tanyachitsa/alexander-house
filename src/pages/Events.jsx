1c1559ccdb33183bd866bf91c4ff5819e8b5003b
Tanyachitsa <165687341+Tanyachitsa@users.noreply.github.com>
2025-04-18 13:16:49 +0100

    updated events.jsx and styling

--- src/pages/Events.jsx
+++ src/pages/Events.jsx
@@ -1,7 +1,43 @@
+import Header from "../components/Header";
+import Footer from "../components/Footer";
 export default function Events() {
   return (
     <>
-      <h1>hi</h1>
+      <Header />
+      <h1
+        style={{
+          textAlign: "center",
+          marginTop: "70px",
+          marginBottom: "30px",
+        }}
+      >
+        - Events -
+      </h1>
+      <p
+        style={{
+          marginLeft: "300px",
+          marginRight: "300px",
+          fontSize: "25px",
+          marginBottom: "70px",
+        }}
+      >
+        From exclusive member events and curated cultural evenings to seasonal
+        celebrations and wellness retreats, Alexander House is always alive with
+        something exceptional. Book your place and be part of the experience.
+        <br /> Looking to host your own gathering? Host with distinction at
+        Alexander House. Whether you're planning an intimate celebration, a
+        corporate gathering, or a creative showcase, our elegant spaces are
+        available for private hire. With bespoke service, beautiful
+        surroundings, and a discreet atmosphere, each house offers the perfect
+        backdrop for memorable moments tailored to your vision.
+      </p>
+      <div className="events-buttons">
+        <button className="whats-on-button" style={{ marginRight: "50px" }}>
+          Whats On
+        </button>
+        <button className="private-hire-button">Private Hire</button>
+      </div>
+      <Footer />
     </>
   );
 }
