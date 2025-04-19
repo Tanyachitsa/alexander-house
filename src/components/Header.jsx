d780bf4d9a99f59402037d1eb9545cdd8bc7f32b
Tanyachitsa <165687341+Tanyachitsa@users.noreply.github.com>
2025-04-19 19:39:33 +0100

    Updated header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,31 +2,34 @@ import { Link } from "react-router-dom";
 
 export default function Header() {
   return (
-    <nav style={{ marginBottom: "30px" }}>
+    <nav>
       <img
         className="logo"
         src="/src/Media/AlexanderHouse.png"
         alt="Alexander House logo"
       />
       <ul>
-        <Link to="/">
+        <Link to="/" style={{ textDecorationLine: "none" }}>
           <li>Home</li>
         </Link>
-        <Link to="/Houses">
+        <Link to="/Houses" style={{ textDecorationLine: "none" }}>
           <li>Houses</li>
         </Link>
-        <Link to="/Wellness">
+        <Link to="/Wellness" style={{ textDecorationLine: "none" }}>
           <li>Wellness</li>
         </Link>
-        <Link to="/Events">
+        <Link to="/Events" style={{ textDecorationLine: "none" }}>
           <li>Events</li>
         </Link>
-        <Link to="/Membership">
+        <Link to="/Membership" style={{ textDecorationLine: "none" }}>
           <li>Membership</li>
         </Link>
       </ul>
-      <Link to="/SignIn">
-        <span className="sign-in">Sign in</span>
+      <Link
+        to="/SignIn"
+        style={{ textDecorationLine: "none", color: "#2A2A2A" }}
+      >
+        <div className="sign-in">Sign in</div>
       </Link>
     </nav>
   );
