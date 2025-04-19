7af4d0dca63ee73b50e7179ce17ef5bde04871a8
Tanyachitsa <165687341+Tanyachitsa@users.noreply.github.com>
2025-04-19 19:41:08 +0100

    Updated House.jsx

--- src/pages/Houses.jsx
+++ src/pages/Houses.jsx
@@ -5,9 +5,7 @@ export default function Houses() {
   return (
     <>
       <Header />
-      <h1 style={{ textAlign: "center", marginTop: "50px", color: "#4E3D3A" }}>
-        - Houses -
-      </h1>
+      <h1 style={{ textAlign: "center", marginTop: "50px" }}>- Houses -</h1>
       <p
         style={{
           textAlign: "center",
