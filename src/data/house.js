ab42f3d9a068f1a8e0408a6a2b098c12621a7719
Tanyachitsa <165687341+Tanyachitsa@users.noreply.github.com>
2025-04-20 15:30:55 +0100

    Created house.js for data

---
+++ src/data/house.js
@@ -0,0 +1,92 @@
+export const europeHouses = [
+  {
+    id: 1,
+    name: "The Guilded Room",
+    location: "London",
+    image: "/src/Media/houseMedia/london.jpg",
+    info: "Golden accents and plush finishes evoke timeless elegance and classic comfort.",
+  },
+  {
+    id: 2,
+    name: "The Library House",
+    location: "Oxford",
+    image: "/src/Media/houseMedia/oxford.jpg",
+    info: "Warm, book-lined walls and a fireplace — perfect for quiet thinkers and dreamers.",
+  },
+  {
+    id: 3,
+    name: "The Artisan Studio",
+    location: "Paris",
+    image: "/src/Media/houseMedia/paris.jpg",
+    info: "Textured walls, earthy palettes, and vintage furniture offer handcrafted comfort.",
+  },
+  {
+    id: 4,
+    name: "The Roman Garden Suite",
+    location: "Italy",
+    image: "/src/Media/houseMedia/lakecomo.jpg",
+    info: "Classical details meet lush greenery in timeless style.",
+  },
+
+  {
+    id: 5,
+    name: "The Glacier Loft",
+    location: "Switzerland",
+    image: "/src/Media/houseMedia/swissalps.jpg",
+    info: "Mountain luxury with plush linens and fur throws.",
+  },
+  {
+    id: 6,
+    name: "The Tuscan Retreat",
+    location: "Italy",
+    image: "/src/Media/houseMedia/tuscany.jpg",
+    info: "Warm stone walls, rustic charm, and golden light.",
+  },
+];
+
+export const africaHouses = [
+  {
+    id: 1,
+    name: "The Marrakesh Terrace",
+    location: "Morocco",
+    image: "/src/Media/houseMedia/morocco.jpg",
+    info: "Vibrant mosaics and lantern-lit ambiance.",
+  },
+  {
+    id: 2,
+    name: "The Baobab Suite",
+    location: "zimbabwe",
+    image: "/src/Media/houseMedia/zimbabwe.jpeg",
+    info: "Rooted in strength and beauty, with grounding, natural decor.",
+  },
+  {
+    id: 3,
+    name: "The Oasis Pavilion",
+    location: "Egypt",
+    image: "/src/Media/houseMedia/egypt.jpg",
+    info: "Fragrant, peaceful, and filled with quiet romance.",
+  },
+  {
+    id: 4,
+    name: "The Hibiscus Hideaway",
+    location: "Mauritius",
+    image: "/src/Media/houseMedia/mauritius.jpg",
+    info: "Vibrant, floral, and intimate — a bold yet calming retreat.",
+  },
+];
+export const otherHouses = [
+  {
+    id: 1,
+    name: "The Palm Pavilion",
+    location: "St Lucia",
+    image: "/src/Media/houseMedia/stlucia.jpg",
+    info: "A tranquil escape surrounded by lush greenery and natural wood finishes.",
+  },
+  {
+    id: 2,
+    name: "The Medina Room",
+    location: "Jordan",
+    image: "/src/Media/houseMedia/jordan.jpg",
+    info: "Tiled elegance and intimate courtyards create a hidden haven.",
+  },
+];
